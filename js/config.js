7aa39e8db524ee33c29f32c568755d42e090c03b
Ozmundooo <thefatwiz@gmail.com>
2018-04-30 22:50:01 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "drippycasesmain@gmail.com"
+	        email: "you@yours.com"
 	    },
 
 	    // set the currency, see the currency reference for more info
