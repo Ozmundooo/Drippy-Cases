3deec1c08b5a46f49ad8834c400c3a5e3d548f5c
Ozmundooo <thefatwiz@gmail.com>
2018-05-04 19:23:20 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "drippycasesmail@gmail.com"
+	        email: "drippycasesmain@gmail.com"
 	    },
 
 	    // set the currency, see the currency reference for more info
